6d70da8e53ea8c10f7a8c418795b1bceb73eb3b7
payamrastogi <payamrastogi@gmail.com>
2015-10-19 18:42:15 -0400

    updated: BM25 according to slide

--- src/main/java/com/wse/ranking/BM25.java
+++ src/main/java/com/wse/ranking/BM25.java
@@ -5,52 +5,36 @@ package com.wse.ranking;
 /* 
  * http://xapian.org/docs/bm25.html	
  * 
- * k1, k3 are constants,
-	q is the wqf, the within query frequency,
-	f is the wdf, the within document frequency,
-	n is the number of documents in the collection indexed by this term,
-	N is the total number of documents in the collection,
-	r is the number of relevant documents indexed by this term,
-	R is the total number of relevant documents,
-	L is the normalised document length (i.e. the length of this document divided by the average length 
-	of documents in the collection).
-	
-	k1=1, k2=0, k3=1, and b=0.5.
+ * N: total number of documents in the collection;
+• ft : number of documents that contain term t;
+• fd,t: frequency of term t in document d;
+• |d|: length of document d;
+• |d|avg: the average length of documents in the collection;
+• k1 and b: constants, usually k1 = 1.2 and b = 0.75 
  */
 public class BM25 
 {
-	private final static int k1 = 1;
-	private final static int k2 = 0;
-	private final static int k3 = 1;
-	private final static double b = 0.5;
+	private final static double k1 = 1.2;
+	private final static double b = 0.75;
 	
-	private int q;
 	private int N;
-	private int R;
-	private int a; //avg length of documents in the collection;
+	private double dAvg; //avg length of documents in the collection;
 	
-	private double X;
-	
-	public BM25(int q, int N, int a, int R)
+	public BM25(int N, double dAvg)
 	{
-		this.q = q;
 		this.N = N;
-		this.a = a;
-		this.R = R;
-		this.X = ((k3 + 1) * this.q)/(k3+q);
+		this.dAvg = dAvg;
 	}
 	
-	public double getScore(int f, int n, int r, int l)
+	public double getScore(int ft, int fdt, int d)
 	{
-		double L = l/this.a;
-		double K = k1 * (b * L  + (1-b));
+		double score = 0.0;
+		double K = k1 * (1-b) + b * (d/this.dAvg);
 		
-		double Y = ((k1+1)*f)/(K + f);
-		
-		double z = ((r+0.5) * (this.N - n - this.R + r + 0.5))/((n-r+0.5)*(this.R-r+0.5));
-		double Z = Math.log(z);
-		
-		return X * Y * Z;
+		double X = (N - ft + 0.5)/ft+0.5;
+		double Y = ((k1 + 1) * fdt) / (K + fdt);
+		score = Math.log(X * Y);
+		return score;
 	}
 
 }
